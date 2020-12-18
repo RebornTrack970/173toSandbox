b0499e32a5bce2bd7b502e8079bae8a574527f73
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2020-12-18 10:00:14 +0300

    added

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+Vars.enableConsole = true;
+
+var mymapd = SectorPreset("testground", Planets.serpulo, 173);
+mymapd.alwaysUnlocked = true;
+mymapd.captureWave = 1;
+mymapd.difficulty = 1;
+mymapd.useAI = false;
